f6488ea79312fdfb80ac0d4121b9ce3aeec2213a
ismail codar <icodar@gmail.com>
2018-12-06 00:04:59 +0300

    props-ref-1 test

---
+++ .packages/babel-plugin-transform-fidan-jsx/test/fixtures/props-ref-1/actual.jsx
@@ -0,0 +1,6 @@
+const buttonRef1 = null;
+const button1 = (
+	<Button propsRef={buttonRef1} variant="primary">
+		Button 1
+	</Button>
+);
---
+++ .packages/babel-plugin-transform-fidan-jsx/test/fixtures/props-ref-1/expected.js
@@ -0,0 +1,8 @@
+var buttonRef1 = null;
+var button1 = fidan(
+	Button,
+	(propsRef = {
+		variant: 'primary'
+	}),
+	'Button 1'
+);
