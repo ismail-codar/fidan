718681d85d6298b3949b81fb729c6c6d00b5f0d2
ismail codar <icodar@gmail.com>
2020-01-01 15:01:42 +0300

    undefined insert fix

--- packages/fidan-jsx/src/index.ts
+++ packages/fidan-jsx/src/index.ts
@@ -1,134 +1,112 @@
-import {
-  compute,
-  FidanValue,
-  htmlProps,
-  arrayMap as fidanArrayMap
-} from "@fidanjs/runtime";
+import { compute, FidanValue, htmlProps, arrayMap as fidanArrayMap } from '@fidanjs/runtime';
 
-export * from "./events";
-export * from "./array-map";
+export * from './events';
+export * from './array-map';
 
 export const arrayMap = (
-  parent: Node & ParentNode,
-  arr: FidanValue<any[]>,
-  renderCallback?: (item: any, idx?: number) => any,
-  marker?: Element
+	parent: Node & ParentNode,
+	arr: FidanValue<any[]>,
+	renderCallback?: (item: any, idx?: number) => any,
+	marker?: Element
 ) => {
-  fidanArrayMap(arr, parent, marker, renderCallback, "reconcile");
+	fidanArrayMap(arr, parent, marker, renderCallback, 'reconcile');
 };
 
-export const insert = (
-  parent: Node,
-  accessor: any,
-  init?: any,
-  marker?: Node
-) => {
-  if (typeof accessor === "object") {
-    if (Array.isArray(accessor)) {
-      accessor.forEach(function(item) {
-        parent.insertBefore(item, marker);
-      });
-    } else if (accessor instanceof Node) {
-      parent.insertBefore(accessor, marker);
-    }
-  } else if (typeof accessor === "function") {
-    const node = document.createTextNode("");
-    if (accessor.hasOwnProperty("$val")) {
-      compute(() => {
-        node.data = accessor();
-        if (!node.parentNode) {
-          parent.insertBefore(node, marker);
-        }
-      });
-    } else {
-      node.data = accessor();
-      parent.insertBefore(node, marker);
-    }
-  } else {
-    const node = document.createTextNode(accessor);
-    parent.insertBefore(node, marker);
-  }
+export const insert = (parent: Node, accessor: any, init?: any, marker?: Node) => {
+	if (typeof accessor === 'object') {
+		if (Array.isArray(accessor)) {
+			accessor.forEach(function(item) {
+				parent.insertBefore(item, marker);
+			});
+		} else if (accessor instanceof Node) {
+			parent.insertBefore(accessor, marker);
+		}
+	} else if (typeof accessor === 'function') {
+		const node = document.createTextNode('');
+		if (accessor.hasOwnProperty('$val')) {
+			compute(() => {
+				node.data = accessor();
+				if (!node.parentNode) {
+					parent.insertBefore(node, marker);
+				}
+			});
+		} else {
+			node.data = accessor();
+			parent.insertBefore(node, marker);
+		}
+	} else if (accessor !== undefined) {
+		const node = document.createTextNode(accessor);
+		parent.insertBefore(node, marker);
+	}
 };
 
 export const spread = (node: HTMLElement, accessor: any) => {
-  if (typeof accessor === "function") {
-    accessor(node);
-  } else {
-    if (typeof accessor === "object") {
-      for (var key in accessor) {
-        accessor[key] != null &&
-          attr(
-            node,
-            key,
-            !htmlProps[key] || key.indexOf("-") !== -1,
-            accessor[key]
-          );
-      }
-      return;
-    } else if (accessor instanceof Node === false)
-      accessor = document.createTextNode(accessor || "");
-    node.appendChild(accessor);
-  }
+	if (typeof accessor === 'function') {
+		accessor(node);
+	} else {
+		if (typeof accessor === 'object') {
+			for (var key in accessor) {
+				accessor[key] != null && attr(node, key, !htmlProps[key] || key.indexOf('-') !== -1, accessor[key]);
+			}
+			return;
+		} else if (accessor instanceof Node === false) accessor = document.createTextNode(accessor || '');
+		node.appendChild(accessor);
+	}
 };
 
-export const attr = (
-  node: Element,
-  attributeName: string,
-  setAttr: boolean,
-  cmp: any
-) => {
-  if (cmp.hasOwnProperty("$val")) {
-    if (setAttr) {
-      compute(() => node.setAttribute(attributeName, cmp()), [cmp]);
-    } else {
-      compute(() => (node[attributeName] = cmp()), [cmp]);
-    }
-  } else {
-    if (typeof cmp === "function") {
-      cmp = cmp(node);
-    }
-    if (setAttr) {
-      node.setAttribute(attributeName, cmp);
-    } else {
-      node[attributeName] = cmp;
-    }
-  }
+export const attr = (node: Element, attributeName: string, setAttr: boolean, cmp: any) => {
+	if (cmp.hasOwnProperty('$val')) {
+		if (setAttr) {
+			compute(() => node.setAttribute(attributeName, cmp()), [ cmp ]);
+		} else {
+			compute(() => (node[attributeName] = cmp()), [ cmp ]);
+		}
+	} else {
+		if (typeof cmp === 'function') {
+			cmp = cmp(node);
+		}
+		if (setAttr) {
+			node.setAttribute(attributeName, cmp);
+		} else {
+			node[attributeName] = cmp;
+		}
+	}
 };
 
 export const conditional = (
-  parent: Node & ParentNode,
-  condition: {
-    test: () => boolean | FidanValue<any>;
-    consequent: any;
-    alternate: any;
-  },
-  init?: any,
-  marker?: Node
+	parent: Node & ParentNode,
+	condition: {
+		test: () => boolean | FidanValue<any>;
+		consequent: any;
+		alternate: any;
+	},
+	init?: any,
+	marker?: Node
 ) => {
-  let oldElement = null;
-  let lastVal = false;
-  const conditionCompute = condition.test.hasOwnProperty("$val")
-    ? condition.test
-    : compute(condition.test);
-  compute(() => {
-    if (oldElement && parent.childElementCount === 0) {
-      return;
-    }
-    const val = !!conditionCompute();
-    if (val !== lastVal) {
-      if (parent && parent.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
-        parent = marker.parentNode;
-      }
-      let newElement = val ? condition.consequent : condition.alternate;
-      if (newElement instanceof Node === false)
-        newElement = document.createTextNode(newElement || "");
-      if (oldElement) {
-        parent.replaceChild(newElement, oldElement);
-      } else {
-        parent.insertBefore(newElement, marker);
-      }
-      oldElement = newElement;
-    }
-    lastVal = val;
-  }, [conditionCompute]);
+	let oldElement = null;
+	let lastVal = false;
+	const conditionCompute = condition.test.hasOwnProperty('$val') ? condition.test : compute(condition.test);
+	compute(
+		() => {
+			if (oldElement && parent.childElementCount === 0) {
+				return;
+			}
+			const val = !!conditionCompute();
+			if (val !== lastVal) {
+				if (parent && parent.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
+					parent = marker.parentNode;
+				}
+				let newElement = val ? condition.consequent : condition.alternate;
+				if (newElement instanceof Node === false) newElement = document.createTextNode(newElement || '');
+				if (oldElement) {
+					parent.replaceChild(newElement, oldElement);
+				} else {
+					parent.insertBefore(newElement, marker);
+				}
+				oldElement = newElement;
+			}
+			lastVal = val;
+		},
+		[ conditionCompute ]
+	);
 };
