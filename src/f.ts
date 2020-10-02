86add485175c8c25ab7a7f3352bcc1e2279537f6
ismail codar <icodar@gmail.com>
2020-10-02 14:20:53 +0300

    overrideArrayMutators

--- src/f.ts
+++ src/f.ts
@@ -1,4 +1,4 @@
-import { FidanValue, ComputionMethodArguments, FidanValueFn } from '.';
+import { FidanValue, ComputionMethodArguments, FidanValueFn, FidanArray } from '.';
 
 let autoTracks: any[] = null;
 
@@ -10,7 +10,9 @@ export const value = <T>(val?: T): FidanValueFn<T> => {
 			return innerFn['$val'];
 		} else {
 			const updateAfter = Array.isArray(val);
-			if (!updateAfter) innerFn['$val'] = val;
+			if (!updateAfter) {
+				innerFn['$val'] = val;
+			}
 			let depends: FidanValue<any>[] = innerFn['c_depends'];
 			if (depends.length)
 				for (var i = 0; i < depends.length; i++) {
@@ -20,7 +22,10 @@ export const value = <T>(val?: T): FidanValueFn<T> => {
 						depends[i](innerFn.$val, innerFn);
 					}
 				}
-			if (updateAfter) innerFn['$val'] = val;
+			if (updateAfter) {
+				innerFn['$val'] = val;
+				overrideArrayMutators(innerFn);
+			}
 		}
 	};
 
@@ -54,9 +59,35 @@ export const computed = <T>(fn: (val: T, opt?: ComputionMethodArguments<T>) => a
 	const cmp = value<T>(undefined);
 	const val = fn(undefined, { caller: cmp } as any);
 	cmp.$val = val;
+	if (Array.isArray(val)) {
+		overrideArrayMutators(cmp as any);
+	}
 	const deps = autoTracks ? autoTracks : dependencies;
 	autoTracks = null;
 	cmp['compute'] = fn;
 	for (var i = 0; i < deps.length; i++) deps[i]['c_depends'].push(cmp);
 	return cmp;
+};
+
+const overrideArrayMutators = (dataArray: FidanArray<any[]>) => {
+	if (!dataArray.size) dataArray.size = value(dataArray.$val.length);
+	else dataArray.size(dataArray.$val.length);
+	if (dataArray.$val['$overrided']) return;
+	dataArray.$val['$overrided'] = true;
+	[ 'copyWithin', 'fill', 'pop', 'push', 'reverse', 'shift', 'sort', 'splice', 'unshift' ].forEach((method) => {
+		dataArray.$val[method] = function() {
+			const arr = dataArray.$val.slice(0);
+			const size1 = arr.length;
+			const ret = Array.prototype[method].apply(arr, arguments);
+			const size2 = arr.length;
+			if (size1 !== size2) dataArray.size(size2);
+			// TODO event based strategy for -> 'pop, push, shift, splice, unshift'
+			dataArray(arr, {
+				method,
+				caller: dataArray,
+				args: [ ...arguments ]
+			});
+			return ret;
+		};
+	});
 };
