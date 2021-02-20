31b13c580e2cee573b3d93da985ba5a9e6c42497
ismail codar <icodar@gmail.com>
2021-02-20 22:25:46 +0300

    todomvc wip

---
+++ src/_demo/todomvc.ts
@@ -0,0 +1,227 @@
+import { FidanValue, FidanArray, value, html, debounce } from '../';
+import { computed } from '../f';
+
+// interface & types
+type FilterType = '' | 'active' | 'completed';
+interface Todo {
+  id: number;
+  title: FidanValue<string>;
+  editing: FidanValue<boolean>;
+  completed: FidanValue<boolean>;
+}
+
+// variables
+const STORAGE_KEY = 'fidan_todomvc';
+const hashFilter = value<FilterType>('');
+const todos = value<Todo[]>([
+  {
+    id: 1,
+    title: value('test1'),
+    completed: value(false),
+    editing: value(false),
+  },
+  {
+    id: 2,
+    title: value('test'),
+    completed: value(false),
+    editing: value(false),
+  },
+]) as FidanArray<Todo[]>;
+const allChecked = value(false);
+
+const shownTodos: FidanArray<Todo[]> = computed(() => {
+  let _todos = todos();
+  const filter = hashFilter();
+  if (filter !== '') {
+    _todos = _todos.filter(todo =>
+      filter === 'active' ? !todo.completed() : todo.completed()
+    ) as any;
+  }
+  return _todos;
+});
+
+// methods
+const updateTodo = (todo: Todo, title: string) => {
+  title = title.trim();
+  if (title) {
+    todo.title(title);
+    todo.editing(false);
+  } else {
+    removeTodo(todo.id);
+  }
+};
+const removeTodo = id => {
+  todos().splice(
+    shownTodos().findIndex(item => item.id == id),
+    1
+  );
+};
+const clearCompleted = e => {
+  const removes = [];
+  todos().forEach(todo => {
+    if (todo.completed()) removes.push(todo);
+  });
+  while (removes.length) todos().splice(todos().indexOf(removes.pop()), 1);
+};
+
+// css computations
+const footerLinkCss = (waiting: FilterType) =>
+  computed(() => (hashFilter() === waiting ? 'selected' : ''));
+
+const editItemCss = (todo: Todo) =>
+  computed(() => {
+    const classes = [];
+    todo.completed() && classes.push('completed');
+    todo.editing() && classes.push('editing');
+    return classes.join(' ');
+  });
+
+// footer
+const todoCount = computed(() => {
+  const count = todos().filter(item => !item.completed()).length;
+  if (count === 0 && !allChecked()) {
+    allChecked(true);
+  }
+  if (count && allChecked()) {
+    allChecked(false);
+  }
+  return count;
+}, [todos.size]);
+
+// router
+window.addEventListener('hashchange', () => {
+  hashFilter(window.location.hash.substr(2) as FilterType);
+});
+hashFilter(window.location.hash.substr(2) as FilterType);
+
+// storage
+const saveTodo = computed(
+  debounce(() => {
+    const strTodos = JSON.stringify(todos());
+    localStorage.setItem(STORAGE_KEY, strTodos);
+  }, 0),
+  [todoCount]
+);
+const _savedTodos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+_savedTodos.forEach(item => {
+  item.title = value(item.title);
+  item.editing = value(false).depends(() => [saveTodo]);
+  item.completed = value(item.completed).depends(() => [todoCount]);
+});
+todos(_savedTodos);
+allChecked(todoCount() === 0);
+
+// view
+const APP = html`
+  <div>
+    <header class="header">
+      <h1>todos</h1>
+      <input
+        class="new-todo"
+        placeholder="What needs to be done?"
+        autofocus
+        onkeypress="${e => {
+          if (e.key === 'Enter') {
+            const title = e.target.value.trim();
+            title &&
+              todos().push({
+                id: Math.random(),
+                title: value(title).depends(() => [saveTodo]),
+                editing: value(false),
+                completed: value(false).depends(() => [todoCount]),
+              });
+            e.target.value = '';
+          }
+        }}"
+      />
+    </header>
+    ${computed(() => {
+      if (todos.size() > 0) {
+        return html`
+          <section class="main">
+            <input
+              id="toggle-all"
+              class="toggle-all"
+              type="checkbox"
+              checked="${allChecked}"
+              onclick="${e =>
+                todos().forEach(todo => todo.completed(e.target.checked))}"
+            />
+            <label for="toggle-all">Mark all as complete</label>
+            <ul class="todo-list">
+              ${shownTodos.map(
+                todo => html`
+                  <li
+                    class="${editItemCss(todo)}"
+                    ondblclick="${(e: any) => {
+                      todo.editing(true);
+                      e.currentTarget.lastElementChild.focus();
+                    }}"
+                  >
+                    <div class="view">
+                      <input
+                        class="toggle"
+                        type="checkbox"
+                        onchange="${e => {
+                          todo.completed(e.target.checked);
+                        }}"
+                        checked="${todo.completed}"
+                      />
+                      <label>${todo.title}</label>
+                      <button
+                        class="destroy"
+                        onclick="${e => removeTodo(todo.id)}"
+                      ></button>
+                    </div>
+                    <input
+                      class="edit"
+                      value="${todo.title}"
+                      onkeypress="${e => {
+                        if (e.key === 'Enter') {
+                          updateTodo(todo, e.target.value);
+                        }
+                      }}"
+                      onblur="${e => updateTodo(todo, e.target.value)}"
+                    />
+                  </li>
+                `
+              )}
+            </ul>
+          </section>
+          <footer class="footer">
+            <span class="todo-count"
+              ><strong>${todoCount}</strong> item${computed(() =>
+                todoCount() > 1 ? 's' : ''
+              )}
+              left</span
+            >
+            <ul class="filters">
+              <li>
+                <a class="${footerLinkCss('')}" href="#/">All</a>
+              </li>
+              <li>
+                <a class="${footerLinkCss('active')}" href="#/active">Active</a>
+              </li>
+              <li>
+                <a class="${footerLinkCss('completed')}" href="#/completed"
+                  >Completed</a
+                >
+              </li>
+            </ul>
+            ${computed(() => {
+              if (todos().length - todoCount() > 0) {
+                return html`
+                  <button class="clear-completed" onclick="${clearCompleted}">
+                    Clear completed
+                  </button>
+                `;
+              }
+            })}
+          </footer>
+        `;
+      }
+    })}
+  </div>
+`;
+
+document.getElementById('main').appendChild(APP);
--- src/html.ts
+++ src/html.ts
@@ -13,217 +13,233 @@ const ARRAY = 16;
 const TEXT_OR_DOM = TEXT | DOM;
 
 export const htmlProps = {
-	id: true,
-	nodeValue: true,
-	textContent: true,
-	className: true,
-	innerHTML: true,
-	innerText: true,
-	tabIndex: true,
-	value: true,
-	checked: true,
-	disabled: true,
-	readonly: true,
-	contentEditable: true
+  id: true,
+  nodeValue: true,
+  textContent: true,
+  className: true,
+  innerHTML: true,
+  innerText: true,
+  tabIndex: true,
+  value: true,
+  checked: true,
+  disabled: true,
+  readonly: true,
+  contentEditable: true,
 };
 
 let template = document.createElement('template');
 
 export const html = (literals, ...vars): DocumentFragment => {
-	let raw = literals.raw,
-		result = '',
-		i = 0,
-		len = vars.length,
-		str = '';
+  let raw = literals.raw,
+    result = '',
+    i = 0,
+    len = vars.length,
+    str = '';
 
-	while (i < len) {
-		str = raw[i];
-		if (str.startsWith('"')) {
-			str = str.substr(1);
-		}
-		if (raw[i].endsWith('="')) {
-			//attributes
-			var p = str.lastIndexOf(' ') + 1;
-			var attr = str.substr(p, str.length - p - 2);
-			p = str.lastIndexOf('<');
-			var comment = '<!--$cmt_' + attr + '-->';
-			if (p === -1) {
-				//next attributes
-				p = result.lastIndexOf('<');
-				result =
-					result.substr(0, p) +
-					comment +
-					result.substr(p) +
-					str.substr(0, str.length - attr.length - 3).trim();
-			} else {
-				// fist attribute
-				result += str.substr(0, p) + comment + str.substr(p, str.length - p - attr.length - 3).trim();
-			}
-		} else {
-			//text nodes
-			result += str + '<!--$cmt' + '-->';
-		}
-		i++;
-	}
-	let extra = raw[raw.length - 1];
-	if (extra.startsWith('"')) {
-		extra = extra.substr(1);
-	}
-	result += extra;
+  while (i < len) {
+    str = raw[i];
+    if (str.startsWith('"')) {
+      str = str.substr(1);
+    }
+    if (raw[i].endsWith('="')) {
+      //attributes
+      var p = str.lastIndexOf(' ') + 1;
+      var attr = str.substr(p, str.length - p - 2);
+      p = str.lastIndexOf('<');
+      var comment = '<!--$cmt_' + attr + '-->';
+      if (p === -1) {
+        //next attributes
+        p = result.lastIndexOf('<');
+        result =
+          result.substr(0, p) +
+          comment +
+          result.substr(p) +
+          str.substr(0, str.length - attr.length - 3).trim();
+      } else {
+        // fist attribute
+        result +=
+          str.substr(0, p) +
+          comment +
+          str.substr(p, str.length - p - attr.length - 3).trim();
+      }
+    } else {
+      //text nodes
+      result += str + '<!--$cmt' + '-->';
+    }
+    i++;
+  }
+  let extra = raw[raw.length - 1];
+  if (extra.startsWith('"')) {
+    extra = extra.substr(1);
+  }
+  result += extra;
 
-	template = template.cloneNode(false) as HTMLTemplateElement;
-	template.innerHTML = result;
+  template = template.cloneNode(false) as HTMLTemplateElement;
+  template.innerHTML = result;
+  console.log(result);
 
-	const element = template.content;
-	const commentNodes = [];
-	walkForCommentNodes(element, commentNodes);
-	updateNodesByCommentNodes(commentNodes, vars);
+  const element = template.content;
+  const commentNodes = [];
+  walkForCommentNodes(element, commentNodes);
+  updateNodesByCommentNodes(commentNodes, vars);
 
-	return element;
+  return element;
 };
 
 const walkForCommentNodes = (element, commentNodes) => {
-	var treeWalker = document.createTreeWalker(
-		element,
-		NodeFilter.SHOW_COMMENT,
-		{
-			acceptNode: function(node) {
-				var nodeValue = node.nodeValue.trim();
-				return nodeValue.startsWith('$cmt') ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
-			}
-		},
-		false
-	);
+  var treeWalker = document.createTreeWalker(
+    element,
+    NodeFilter.SHOW_COMMENT,
+    {
+      acceptNode: function(node) {
+        var nodeValue = node.nodeValue.trim();
+        return nodeValue.startsWith('$cmt')
+          ? NodeFilter.FILTER_ACCEPT
+          : NodeFilter.FILTER_REJECT;
+      },
+    },
+    false
+  );
 
-	while (treeWalker.nextNode()) {
-		commentNodes.push(treeWalker.currentNode);
-	}
+  while (treeWalker.nextNode()) {
+    commentNodes.push(treeWalker.currentNode);
+  }
 };
 
 const updateNodesByCommentNodes = (commentNodes: Comment[], params: any[]) => {
-	for (var i = 0; i < commentNodes.length; i++) {
-		const commentNode = commentNodes[i];
-		var commentValue = commentNode.nodeValue;
-		let element = null;
-		let attributeName: string = null;
-		let param = params[i];
-		const isDynamic = param && param.hasOwnProperty('$val');
+  for (var i = 0; i < commentNodes.length; i++) {
+    const commentNode = commentNodes[i];
+    var commentValue = commentNode.nodeValue;
+    let element = null;
+    let attributeName: string = null;
+    let param = params[i];
+    const isDynamic = param && param.hasOwnProperty('$val');
 
-		const attrIdx = commentValue.indexOf('_');
-		const paramType =
-			attrIdx !== -1
-				? DOM
-				: typeof param === 'function' && !isDynamic
-					? FN
-					: Array.isArray(param) ? ARRAY : typeof param === 'object' && param ? HTM : TEXT;
+    const attrIdx = commentValue.indexOf('_');
+    const paramType =
+      attrIdx !== -1
+        ? DOM
+        : typeof param === 'function' && !isDynamic
+        ? FN
+        : Array.isArray(param)
+        ? ARRAY
+        : typeof param === 'object' && param
+        ? HTM
+        : TEXT;
 
-		if (paramType & TEXT_OR_DOM) {
-			if (paramType === TEXT) {
-				attributeName = 'textContent';
-				element = document.createTextNode(isDynamic ? param.$val : param);
-				commentNode.parentElement.insertBefore(element, commentNode.nextSibling);
-				if (!isDynamic) {
-					if (Array.isArray(param)) {
-						for (var p = 0; p < param.length; p++) {
-							commentNode.parentElement.appendChild(param[p]);
-						}
-					}
-				}
-			} else if (paramType === DOM) {
-				attributeName = commentValue.substr(attrIdx + 1);
-				element = commentNode.nextElementSibling;
-			}
-			paramType !== FN && commentNode.remove();
-			if (attributeName.startsWith('on')) {
-				(element as Element).addEventListener(attributeName.substr(2), param);
-			} else if (isDynamic) {
-				if (htmlProps[attributeName]) {
-					computed(
-						() => {
-							element[attributeName] = param();
-						},
-						[ param ]
-					);
-				} else {
-					computed(
-						() => {
-							element.setAttribute(attributeName, param());
-						},
-						[ param ]
-					);
-				}
-			} else {
-				if (htmlProps[attributeName]) {
-					element[attributeName] = param;
-				} else if (typeof param === 'function') {
-					param(element);
-				} else {
-					element.setAttribute(attributeName, param);
-				}
-			}
-		} else if (paramType === FN) {
-			if (commentNode.parentElement) {
-				param(commentNode.parentElement, commentNode.nextElementSibling);
-				commentNode.remove();
-			} else {
-				//conditionalDom can be place on root
-				window.requestAnimationFrame(() => {
-					param(commentNode.parentElement, commentNode.nextElementSibling);
-					commentNode.remove();
-				});
-			}
-		} else if (paramType === ARRAY) {
-			const fragment = document.createDocumentFragment();
-			param.forEach((p) => {
-				fragment.appendChild(p);
-			});
-			commentNode.parentElement.insertBefore(fragment, commentNode.nextSibling);
-		} else if (paramType === HTM) {
-			if (param.renderFn) {
-				arrayMap(
-					param.arr,
-					commentNode.parentElement,
-					commentNode.nextSibling as any,
-					param.renderFn,
-					param.renderMode
-				);
-			} else {
-				commentNode.parentElement.insertBefore(param, commentNode.nextSibling);
-			}
-		}
-	}
+    if (paramType & TEXT_OR_DOM) {
+      if (paramType === TEXT) {
+        attributeName = 'textContent';
+        element = document.createTextNode(isDynamic ? param.$val : param);
+        commentNode.parentElement.insertBefore(
+          element,
+          commentNode.nextSibling
+        );
+        if (!isDynamic) {
+          if (Array.isArray(param)) {
+            for (var p = 0; p < param.length; p++) {
+              commentNode.parentElement.appendChild(param[p]);
+            }
+          }
+        }
+      } else if (paramType === DOM) {
+        attributeName = commentValue.substr(attrIdx + 1);
+        element = commentNode.nextElementSibling;
+      }
+      paramType !== FN && commentNode.remove();
+      if (attributeName.startsWith('on')) {
+        (element as Element).addEventListener(attributeName.substr(2), param);
+      } else if (isDynamic) {
+        if (htmlProps[attributeName]) {
+          computed(() => {
+            element[attributeName] = param();
+          }, [param]);
+        } else {
+          computed(() => {
+            element.setAttribute(attributeName, param());
+          }, [param]);
+        }
+      } else {
+        if (htmlProps[attributeName]) {
+          element[attributeName] = param;
+        } else if (typeof param === 'function') {
+          param(element);
+        } else {
+          element.setAttribute(attributeName, param);
+        }
+      }
+    } else if (paramType === FN) {
+      if (commentNode.parentElement) {
+        param(commentNode.parentElement, commentNode.nextElementSibling);
+        commentNode.remove();
+      } else {
+        //conditionalDom can be place on root
+        window.requestAnimationFrame(() => {
+          param(commentNode.parentElement, commentNode.nextElementSibling);
+          commentNode.remove();
+        });
+      }
+    } else if (paramType === ARRAY) {
+      const fragment = document.createDocumentFragment();
+      param.forEach(p => {
+        fragment.appendChild(p);
+      });
+      commentNode.parentElement.insertBefore(fragment, commentNode.nextSibling);
+    } else if (paramType === HTM) {
+      if (param.renderFn) {
+        arrayMap(
+          param.arr,
+          commentNode.parentElement,
+          commentNode.nextSibling as any,
+          param.renderFn,
+          param.renderMode
+        );
+      } else {
+        commentNode.parentElement.insertBefore(param, commentNode.nextSibling);
+      }
+    }
+  }
 };
 
 export const arrayMap = <T>(
-	arr: FidanValue<T[]>,
-	parentDom: Node & ParentNode,
-	nextElement: Element,
-	renderCallback: (item: any, idx?: number, isInsert?: boolean) => Node,
-	renderMode: 'reuse' | 'reconcile' = 'reconcile'
+  arr: FidanValue<T[]>,
+  parentDom: Node & ParentNode,
+  nextElement: Element,
+  renderCallback: (item: any, idx?: number, isInsert?: boolean) => Node,
+  renderMode: 'reuse' | 'reconcile' = 'reconcile'
 ) => {
-	const prevElement = nextElement ? document.createTextNode('') : undefined;
-	nextElement && parentDom.insertBefore(prevElement, nextElement);
-	computed<any[]>(
-		(nextVal, { caller }) => {
-			const beforeVal = caller.$val;
-			const renderFunction: (parent, renderedValues, data, createFn, noOp, beforeNode?, afterNode?) => void =
-				renderMode === 'reconcile' ? reconcile : reuseNodes;
-			renderFunction(
-				parentDom,
-				beforeVal || [],
-				nextVal || [],
-				(nextItem, index) => {
-					let rendered = renderCallback(nextItem, index) as any;
-					return rendered instanceof Node ? rendered : document.createTextNode(rendered);
-				},
-				() => {},
-				prevElement,
-				nextElement
-			);
-		},
-		[ arr ]
-	);
-	const nextVal = arr.$val.slice(0);
-	arr.$val = [];
-	arr(nextVal);
+  const prevElement = nextElement ? document.createTextNode('') : undefined;
+  nextElement && parentDom.insertBefore(prevElement, nextElement);
+  computed<any[]>(
+    (nextVal, { caller }) => {
+      const beforeVal = caller.$val;
+      const renderFunction: (
+        parent,
+        renderedValues,
+        data,
+        createFn,
+        noOp,
+        beforeNode?,
+        afterNode?
+      ) => void = renderMode === 'reconcile' ? reconcile : reuseNodes;
+      renderFunction(
+        parentDom,
+        beforeVal || [],
+        nextVal || [],
+        (nextItem, index) => {
+          let rendered = renderCallback(nextItem, index) as any;
+          return rendered instanceof Node
+            ? rendered
+            : document.createTextNode(rendered);
+        },
+        () => {},
+        prevElement,
+        nextElement
+      );
+    },
+    [arr]
+  );
+  const nextVal = arr.$val.slice(0);
+  arr.$val = [];
+  arr(nextVal);
 };
