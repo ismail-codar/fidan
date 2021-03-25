b50a43f0b26eefe87ba7038d05a58c30d201b668
ismail codar <icodar@gmail.com>
2021-03-25 19:56:26 +0300

    spread-props..

--- packages/babel-plugin-fidan-jsx/test/fixtures/spread-props/expected.js
+++ packages/babel-plugin-fidan-jsx/test/fixtures/spread-props/expected.js
@@ -9,14 +9,19 @@ export const createOverrides2 = defaultComponent => {
   return [defaultComponent, null];
 };
 const { Label1, labelProps1 } = fidan.computed(() => {
-  return createOverrides1(
+  const { Label1, labelProps1 } = createOverrides1(
     _props => fidan.html`<li __spread="${_props}">${_props.children}</li>`
   );
+  return {
+    Label1: fidan.observable(Label1),
+    labelProps1: fidan.observable(labelProps1),
+  };
 });
 const [Label2, labelProps2] = fidan.computed(() => {
-  return createOverrides2(
+  const [Label2, labelProps2] = createOverrides2(
     _props => fidan.html`<li __spread="${_props}">${_props.children}</li>`
   );
+  return [fidan.observable(Label2), fidan.observable(labelProps2)];
 });
 const element = fidan.html`<ul>${Label1(
   { ...labelProps1 },
